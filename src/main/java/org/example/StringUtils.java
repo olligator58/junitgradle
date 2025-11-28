1366736c75f128c15080473a0107a153acbbd42a
Oleg <olligator58@gmail.com>
2025-11-28 12:16:58 +0300

    PalindromeTest added

---
+++ src/main/java/org/example/StringUtils.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class StringUtils {
+
+    public static boolean isPalindrome(String str) {
+        if (str == null || str.length() < 2) {
+            return false;
+        }
+        str = str.toLowerCase().replaceAll("\\s|'|,", "");
+        System.out.println(str);
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
